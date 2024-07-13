e2d9848aeee51cb55e6003c7f6e48836476c2af9
owias tariq <165525244+1b7owais@users.noreply.github.com>
2024-07-13 18:34:17 +0500

    i push my project

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const rendomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "usernumber",
+        type: "number",
+        message: "Please guess a number"
+    }
+]);
+if (answer.usernumber === rendomNumber) {
+    console.log("congtratulation! you select right number");
+}
+else {
+    console.log("Oops! you select wrong number");
+}
